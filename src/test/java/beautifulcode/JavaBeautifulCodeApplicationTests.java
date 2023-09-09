6bf6c0c81d373470e4069c5eed2697e870e063ab
Ivan Rybnikov <ivan.rybnikov@rt.ru>
2023-09-09 08:19:33 +0300

    initial project

---
+++ src/test/java/beautifulcode/JavaBeautifulCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package beautifulcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBeautifulCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
